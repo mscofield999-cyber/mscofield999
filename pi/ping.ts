51246354fef087afecad2af9636f93061507e61f
mscofield999-cyber <mscofield999@gmail.com>
2025-11-15 21:46:07 +0300

    Create ping.ts

---
+++ pi/ping.ts
@@ -0,0 +1,2 @@
+- import type { VercelRequest, VercelResponse } from '@vercel/node';
+- export default async function handler(req: VercelRequest, res: VercelResponse) { res.setHeader('Content-Type','application/json'); res.status(200).json({ ok: true }); }
